391a1f81e0d63bd126dbc3829e7922406faa188a
Live session user <ubuntu@ubuntu.(none)>
2012-05-01 19:53:39 +0000

    version gráfica a medio funcionar

--- Window.cpp
+++ Window.cpp
@@ -4,38 +4,24 @@
 #include <FL/Fl_Preferences.H>
 #include <FL/fl_draw.H>
 #include <FL/x.H>
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
-
+using namespace std;
 
 #ifndef WIN32
 #  include <unistd.h>
 #  include <sys/time.h>
 #endif // !WIN32
 
+const int BLOCK_COLS=8;
+const int BLOCK_ROWS=8;
+const int BLOCK_SIZE=20;
 
-#define BLOCK_COLS	20
-#define BLOCK_ROWS	10
-#define BLOCK_SIZE	32
-#define BLOCK_BLAST	100
-
-/////////////////////////////////////////////////////////////////////
 class BlockWindow : public Fl_Double_Window {
 public:
-	struct Block {
-		int		color;
-		bool	bomb;
-		int		y;
-	};
-
-	struct Column {
-		int		num_blocks;
-		Block	blocks[BLOCK_ROWS];
-		int		x;
-	};
 
 	BlockWindow(int X, int Y, int W, int H, const char *L = 0);
 	BlockWindow(int W, int H, const char *L = 0);
@@ -47,34 +33,12 @@ public:
 
 private:
 
-	Fl_Button	*help_button_, *play_button_;
-	int		num_columns_;
-	Column	columns_[BLOCK_COLS];
-	int		count_;
-	bool	help_;
-	int		high_score_;
-	float	interval_;
-	int		level_;
-	int		num_colors_;
-	int		opened_columns_;
 	bool	paused_;
-	int		score_;
-	char    title_[255];
-	int		title_y_;
-
 	void	setup();
-	int		bomb(int color);
-	int		click(int col, int row);
+	int	    click(int col, int row);
 	void	reset();
-
-	static Fl_Preferences	prefs_;
-
-	static void	help_cb(Fl_Widget *wi, BlockWindow *bw);
-	static void	play_cb(Fl_Widget *wi, BlockWindow *bw);
 	static void	timeout_cb(BlockWindow *bw);
 };
-
-Fl_Preferences	BlockWindow::prefs_(Fl_Preferences::USER, "fltk.org", "blocks");
 
 
 int main(int argc, char *argv[]) {
@@ -84,7 +48,7 @@ int main(int argc, char *argv[]) {
 	BlockWindow *bw = new BlockWindow(
 		BLOCK_COLS * BLOCK_SIZE,
 		BLOCK_ROWS * BLOCK_SIZE + 20,
-		"Block Attack!"
+		"8x8"
 	);
 
 	bw->show(argc, argv);
@@ -107,68 +71,50 @@ BlockWindow::~BlockWindow() {
 
 void BlockWindow::setup() {
 	reset();
-	prefs_.get("high_score", high_score_, 0);
 	Fl::add_timeout(0.1, (Fl_Timeout_Handler)timeout_cb, (void *)this);
 }
 
-// Tag all blocks connected to the clicked block and return the number
-// of affected blocks
 int BlockWindow::click(int col, int row) {
+	cout << "click"<<endl;
 }
 
 
-// Draw the block window...
 void BlockWindow::draw() {
-	int		j, k, xx, yy;
-	Block		*b;
-	Column	*c;
-
-	// Draw the blocks...
 	fl_color(FL_BLACK);
 	fl_rectf(0, 0, w(), h());
-
+	static int red=0;
 	// Draw the blocks...
-	for (j = num_columns_, c = columns_; j > 0; j --, c ++){
-		for (k = c->num_blocks, b = c->blocks; k > 0; k --, b ++) {
-			xx = w() - c->x;
-			yy = h() - BLOCK_SIZE - b->y;
-			fl_rectf( j*xx, k*yy, xx, yy,  200,0,200);
+	for (int row = 0; row< 8; row++ ){
+		for (int col= 0; col< 8; col++ ){
+			fl_rectf( row*BLOCK_SIZE, col*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE,red,0 ,0);
 		}
 	}
-
-	// Draw the scores and level...
-	char s[255];
-	sprintf(s, " Score: %d", score_);
-	fl_color(FL_WHITE);
-	fl_font(FL_HELVETICA, 14);
-	fl_draw(s, 40, 0, w() - 40, 20, FL_ALIGN_LEFT);
+	red++;
 }
 
 
 int BlockWindow::handle(int event) {
-	int		j, k, mx, my, count;
-	Block		*b;
-	Column	*c;
-
 	if (Fl_Double_Window::handle(event)){
 		return (1);
 	} 
 
-	if (interval_ < 0.0 || paused_){
-		return (0);
-	}
-
+	int mx, my;
 	switch (event) {
 	case FL_PUSH :
 		if(Fl::event_clicks()!=0){
-			//doble click
+			cout << "Doble click" << endl;
+		}else{
+			cout << "Click" << endl;
 		}
 		mx    = w() - Fl::event_x() + BLOCK_SIZE;
 		my    = h() - Fl::event_y();
-		return (1);
+		//return (1);
+		break;
 	case FL_DRAG:
+		cout << "Arrastrando" << endl;
 		break;
 	case FL_RELEASE:
+		cout << "Soltando" << endl;
 		break;
 	}
 	return (0);
@@ -178,16 +124,6 @@ int BlockWindow::handle(int event) {
 
 // Initialize the block window...
 void BlockWindow::reset() {
-	count_       = 0;
-	help_        = false;
-	interval_    = -1.0;
-	level_       = 1;
-	num_colors_  = 3;
-	num_columns_ = 0;
-	paused_      = false;
-	score_       = 0;
-	title_[0]    = '\0';
-	title_y_     = 0;
 }
 
 
@@ -195,30 +131,13 @@ void BlockWindow::reset() {
 void BlockWindow::new_game() {
 	srand(time(NULL));
 	reset();
-	interval_       = 0.1;
-	opened_columns_ = 0;
-	strcpy(title_, "Level: 1");
-	title_y_ = h();
 	redraw();
 }
 
 
 // Animate the game...
 void BlockWindow::timeout_cb(BlockWindow *bw) {
-	int		i, j;
-	Block	*b;
-	Column	*c;
-	int		lastx, lasty;
-
-	// Update blocks that have been destroyed...
-	for (i = 0, c = bw->columns_; i < bw->num_columns_; i ++, c ++){
-		for (j = 0, b = c->blocks; j < c->num_blocks; j ++, b ++){
-			if (b->color > (BLOCK_BLAST + 1)) {
-				bw->redraw();
-			}
-		}
-	}
-
+	bw->redraw();
 	Fl::repeat_timeout(0.1, (Fl_Timeout_Handler)timeout_cb, (void *)bw);
 }
 
