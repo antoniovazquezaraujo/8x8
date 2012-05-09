fc9221d70f5cca380f0d062920159efc0664e72e
Live session user <ubuntu@ubuntu.(none)>
2012-05-09 23:51:42 +0000

    Experimento calculadora

--- Demo1.cpp
+++ Demo1.cpp
@@ -19,17 +19,147 @@ Demo1::Demo1(TabletView * view)
 
 }
 void Demo1::setup(){
-	for(int col =0; col <COLS; col++){
-		for(int row = 0; row < ROWS; row++){ 
-			model->addBox(0, col, row, 1,1, false);
-			model->lastBox(0).addColorChange(     0, col*32,    128,      0,      0,      0, 100);
-			model->lastBox(0).addColorChange(col*32, col*32,      0, row*32,      0,      0, 100);
-			model->lastBox(0).addColorChange(col*32,      0, row*32,    128,      0, col*32, 100);
-			model->lastBox(0).addColorChange(     0, col*32,      0, row*32, col*32, row*32, 100);
-			model->lastBox(0).addColorChange(col*32, col*32, row*32,      0, col*32,    128, 100);
-			model->lastBox(0).startColorChanges();
-		}
-	}
+	int apagado = 10;
+	model->addBox(0, 0,0, 2,2, true);
+	model->lastBox(0).setColor(0,100,0);
+
+	model->addBox(1, 0,0, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 1,0, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 1,1, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 0,1, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(100,100,100,100,100,100,5);
+	model->lastBox(1).startColorChanges();
+/////////////////////////////////////////////////////////
+	model->addBox(0, 2,0, 2,2, true);
+	model->lastBox(0).setColor(100,0,0);
+
+	model->addBox(1, 2,0, 1,1, true);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 3,0, 1,1, true);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 3,1, 1,1, true);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 2,1, 1,1, true);
+	model->lastBox(1).addColorChange(120,120,120,120,120,120,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+/////////////////////////////////////////////////////////
+	model->addBox(0, 4,0, 2,2, true);
+	model->lastBox(0).setColor(0,0,100);
+
+	model->addBox(1, 4,0, 1,1, true);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 5,0, 1,1, true);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 5,1, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 4,1, 1,1, true);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(140,140,140,140,140,140,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,apagado);
+	model->lastBox(1).startColorChanges();
+/////////////////////////////////////////////////////////
+	model->addBox(0, 6,0, 2,2, true);
+	model->lastBox(0).setColor(100,100,0);
+	int todos = 10;
+
+	model->addBox(1, 6,0, 1,1, true);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,todos);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,5);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 7,0, 1,1, true);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,todos);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,5);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 7,1, 1,1, true);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,todos);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
+
+	model->addBox(1, 6,1, 1,1, true);
+	model->lastBox(1).addColorChange(160,160,160,160,160,160,todos);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).addColorChange(  0,  0,0,0,0,0,5);
+	model->lastBox(1).startColorChanges();
 }
 void Demo1::start(){
 	view->start();
@@ -38,18 +168,6 @@ void Demo1::stop(){
 	view->stop();
 }
 void Demo1::onClick(int col, int row){
-	static bool added = false;
-	if(!added){
-		model->addBox(1, col, 7, 1,1,false);
-		model->lastBox(1).setColor(64,64,64);
-		model->lastBox(1).addColorChange(255,  0,  0,  0,  0,  0,1);
-		model->lastBox(1).addColorChange(  0,255,  0,  0,  0,  0,1);
-		model->lastBox(1).startColorChanges();
-		model->lastBox(1).addPosChange(0,  -1, 10, 7);
-		model->lastBox(1).startPosChanges();
-		added=true;
-	}
-	model->lastBox(1).setPos(Rect(col,7));
 }
 void Demo1::onRelease(int col, int row){
 }
